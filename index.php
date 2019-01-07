3b6e8c5e9f03e041508a2cffbd78e41ae5afdfa8
Matt Peyton <mattpeyton1@gmail.com>
2019-01-07 15:54:57 -0600

    Added head.php

--- index.php
+++ index.php
@@ -1,17 +1,10 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<meta http-equiv="X-UA-Compatible" content="ie=edge">
-<link rel="stylesheet" href="normalize.css">
-<link rel="stylesheet" href="style.css">
-<link rel="stylesheet" href="https://use.typekit.net/ihu5nyi.css">
-<link rel="stylesheet" href="https://cdn.rawgit.com/michalsnik/aos/2.1.1/dist/aos.css">
-<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
-<script src="https://cdn.rawgit.com/michalsnik/aos/2.1.1/dist/aos.js"></script>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 <title>MJPeyton.com</title>
+<!-- Begin head.php INCLUDE -->
+<?php require_once ('head.php'); ?>
+<!-- End head.php INCLUDE -->
 <style>
 nav a {
 	color: white;
@@ -19,7 +12,6 @@ nav a {
 .hamburger-inner, .hamburger-inner:before, .hamburger-inner:after {
 	background-color: white;
 }
-
 </style>
 </head>
 <div class="picback">
--- portfolio.php
+++ portfolio.php
@@ -1,13 +1,9 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<meta http-equiv="X-UA-Compatible" content="ie=edge">
-<link rel="stylesheet" href="normalize.css">
-<link rel="stylesheet" href="https://use.typekit.net/ihu5nyi.css">
-<link rel="stylesheet" href="style.css">
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<!-- Begin head.php INCLUDE -->
+<?php require_once ('head.php'); ?>
+<!-- End head.php INCLUDE -->
 <title>Portfolio</title>
 </head>
 <header class="header-container">
